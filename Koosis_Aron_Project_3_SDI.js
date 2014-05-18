bd4df13e40d42f93f726fcc880ca2776000ab1aa
Aron Koosis <amoniaave@aol.com>
2014-05-17 23:10:56 -0400

    added files

---
+++ Koosis_Aron_Project_3_SDI.js
@@ -0,0 +1,38 @@
+//Aron Koosis
+//Project 3 SDI
+//1405
+
+
+//Global Variables:
+
+var javaAliens = "The alens from java are trying to make me fail this course.";
+var javaPain = "The pain caused by not getting a passing grade on an assignment.";
+var javaNum = 8;
+
+//Boolean function:
+
+
+javaEvil = confirm ("Are the aliens evil and determined in their quest?");
+if (javaEvil === true) {
+	console.log ("Let us fight back!");
+	}else{
+	console.log("Let us leave.");
+	
+var javaAttack = javaAttack("Please enter number of attacks between 1 and 100.)
+javaAttack = parseInt (javaAttack);
+console.log(javaAttack+javaAttack);
+myPrompt = parseInt(javaAttack);
+
+var painLevel = function(javaAttack, hours worked, grade) {
+var painLevel = javaAttack * hours woked * grade;
+	console.log("The number of attacks multiply the hours worked and multiply the grade equals the amount of pain.");
+	return(painLevel);
+	
+//local variables:
+
+var javaRay = "A weapon used against the aliens.";
+var painReduce = "A system that helps to reduce alien attacks.";
+var focusFactor = "A company working to solve the alien invasion.";	
+	
+
+  	
